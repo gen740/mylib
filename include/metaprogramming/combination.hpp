5fb202898f4b9e27c317f32a7eaa9a07dad77bb4
gen740 <gen740.f@gmail.com>
2023-07-20 22:34:55 +0900

    Initial commit

---
+++ include/metaprogramming/combination.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+template <int N, int K> struct Combination {
+  static constexpr int value =
+      Combination<N - 1, K - 1>::value + Combination<N - 1, K>::value;
+};
+
+template <int N> struct Combination<N, 0> {
+  static constexpr int value = 1;
+};
+
+template <int N> struct Combination<N, N> {
+  static constexpr int value = 1;
+};
+
+template <> struct Combination<0, 0> {
+  static constexpr int value = 1;
+};
---
+++ include/metaprogramming/for.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include <type_traits>
+
+template <int NMax, int MMax, int N = 0, int M = 0> struct ForMN {
+  template <template <int I, int J> class T, typename... Args>
+  __attribute__((always_inline)) static void eval_all(Args &&...args) {
+    T<N, M>::eval(std::forward<Args>(args)...);
+    if constexpr (N == NMax - 1 && M == MMax - 1) {
+      return;
+    } else if constexpr (M == MMax - 1) {
+      ForMN<NMax, MMax, N + 1, N>::template eval_all<T>(
+          std::forward<Args>(args)...);
+    } else {
+      ForMN<NMax, MMax, N, M + 1>::template eval_all<T>(
+          std::forward<Args>(args)...);
+    }
+  }
+
+  template <template <int I, int J> class T, typename... Args>
+  __attribute__((always_inline)) static void eval_half(Args &&...args) {
+    T<N, M>::eval(std::forward<Args>(args)...);
+    if constexpr (N == NMax - 1 && M == MMax - 1) {
+      return;
+    } else if constexpr (M == N) {
+      ForMN<NMax, MMax, N + 1, 0>::template eval_half<T>(
+          std::forward<Args>(args)...);
+    } else {
+      ForMN<NMax, MMax, N, M + 1>::template eval_half<T>(
+          std::forward<Args>(args)...);
+    }
+  }
+};
